996c4819c22f152b1098335e26c696bb08d6def5
Your Name <you@example.com>
2026-09-10 16:21:58 +0100

    ex01 done.

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,13 +1,7 @@
 import datetime as dt_module
-#importing the module directly is safer and won't cause conflicts with local variables or functions.
-def format_ft_time():
-    # Get the current time
-    now = dt_module.datetime.now()
-    random_datetime = dt_module.datetime(2023, 3, 1)
-    print( f"Seconds since {random_datetime.strftime('%B %-d, %Y')}: {(now - random_datetime).total_seconds():,.4f} or {(now - random_datetime).total_seconds():.2e} in scientific notation")
-    print(now.strftime("%b %d %Y"))
-
-    # return now
-
-if __name__ == "__main__":
-    format_ft_time()
+import time
+# Get the current time
+seconds = time.time()
+# Format the output using f-strings
+print( f"Seconds since January 1, 1970: {seconds:,.4f} or {seconds:.2e} in scientific notation")
+print(dt_module.datetime.now().strftime("%b %d %Y"))
