7d3e649e960f6afd7c6ba7ec70f3f239d342ba44
Your Name <you@example.com>
2026-09-10 16:50:46 +0100

    ex01 done.

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,6 +1,6 @@
 import datetime as dt_module
 import time
-# Get the current time
+# Get Epoch time in seconds since January 1, 1970
 seconds = time.time()
 # Format the output using f-strings
 print( f"Seconds since January 1, 1970: {seconds:,.4f} or {seconds:.2e} in scientific notation")
