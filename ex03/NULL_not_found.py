a281571e6450b6ea4492cd4837d281b26bd7c312
Your Name <you@example.com>
2026-09-16 19:22:35 +0100

    ex03 done.

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,14 +1,19 @@
 def NULL_not_found(object: any) -> int:
-    if object is None:
+    if object is None: # means null in other languages.
+        # We use 'is' because None is a singleton and is used memory identity comparison.
+        # (only one instance exists in memory),
+        # checking object is None compares memory identity rather than value equality.
         print (f"Nothing: {object} {type(object)}")
         return 0
-    elif isinstance(object, float) and object != object:
+    elif isinstance(object, float) and object != object: # because naN is not equal to itself
         print (f"Cheese: {object} {type(object)}")
+        # print (f"Is NaN: {object != object}") # should be true
+        # print (f"Object: {object}")
         return 0
     elif type(object) is int and object == 0:
         print (f"Zero: {object} {type(object)}")
         return 0
-    elif type(object) == bool and object == False:
+    elif object is False: # we use 'is' because 0 means false using '=='
         print (f"Fake: {object} {type(object)}")
         return 0
     elif object == "":
